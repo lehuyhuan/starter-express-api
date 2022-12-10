cd0bc06d0977c486aa117dadde77f3cd1ae53f77
Huy Huan <huy.huan@ericsson.com>
2022-12-10 21:47:06 +0700

    update api code

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const app = express()
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    console.log("Just got a request /!")
+    res.send('Simple api - copyright @lehuyhuan!')
 })
+
+app.get('/about', function (req, res) {
+	console.log("Just got a request /about!")
+	res.send("Simple api - copyright @lehuyhuan")
+})
+
+app.get('/sum', function (req, res) {
+  var nums= req.query.birthday.split('');
+  var sum=0;
+  for (var i=0;i<nums.length;i++) {
+        sum+=parseInt(nums[i]);
+  }
+  console.log("TOTAL: " + sum)
+  res.send(sum.toString());
+})
+
 app.listen(process.env.PORT || 3000)
